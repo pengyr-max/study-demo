4c6ed476348489c113e0006543d39d0cf598b7d5
pengyr-max <2452302447@qq.com>
2023-04-27 17:16:41 +0800

    springboot-vue项目demo

---
+++ sprintboot_demo/src/main/java/com/example/demo/SvdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvdemoApplication.class, args);
+	}
+
+}
